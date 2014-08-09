0107f2d486fc505b94ba31e8e7faf60b18bbfb33
gandalfx <gandalf.grey@t-online.de>
2014-08-09 21:14:54 +0200

    using grunt

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+  
+  require('load-grunt-tasks')(grunt);
+  
+  grunt.initConfig({
+    
+    pkg: grunt.file.readJSON('package.json'),
+    
+    default_banner:'/*! <%= pkg.name %> by <%= pkg.author.name %> */\n',
+    
+    dirs: {
+      js:   'src',
+      less: 'src',
+      out:  'min'
+    },
+    
+    jsFiles: [
+      '<%= dirs.js %>/init.js',
+      '<%= dirs.js %>/core.js',
+      '<%= dirs.js %>/style.js',
+      '<%= dirs.js %>/search.js',
+      '<%= dirs.js %>/menu.js',
+      '<%= dirs.js %>/picture.js',
+    ],
+    
+    uglify: {
+      dist: {
+        options: {
+          banner: '<%= default_banner %>'
+        },
+        files: {
+          '<%= dirs.out %>/<%= pkg.name %>.js': '<%= jsFiles %>'
+        }
+      }
+    },
+    
+    concat: {
+      dev: {
+        src: '<%= jsFiles %>',
+        dest: '<%= dirs.out %>/<%= pkg.name %>.js'
+      }
+    },
+    
+    less: {
+      dist: {
+        options: {
+          compress: true,
+          banner: '<%= default_banner %>'
+        },
+        files: {
+          '<%= dirs.out %>/<%= pkg.name %>.css': '<%= dirs.less %>/global.less'
+        }
+      },
+      dev: {
+        files: {
+          '<%= dirs.out %>/<%= pkg.name %>.css': '<%= dirs.less %>/global.less'
+        }
+      }
+    },
+    
+    watch: {
+      stylesheets: {
+        files: '<%= dirs.less %>/**/*.less',
+        tasks: ['less:dev']
+      },
+      scripts: {
+        files: '<%= dirs.js %>/**/*.js',
+        tasks: ['concat:dev']
+      }
+    }
+    
+  });
+  
+  grunt.registerTask('default', ['less:dev',  'concat:dev']);
+  grunt.registerTask('dist',    ['less:dist', 'uglify:dist']);
+};
